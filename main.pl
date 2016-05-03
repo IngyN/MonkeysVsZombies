89af05279848f47e19518bd88e793fe73bfb76f5
IngyN <ingyesa@aucegypt.edu>
2016-05-03 12:14:40 +0200

    HTML GENERATION DONE + CSS LINK

--- main.pl
+++ main.pl
@@ -6,11 +6,18 @@ use IO::Uncompress::Unzip qw(unzip $UnzipError);
 use XML::LibXML;
 #use XML::LibXML::Documents;
 
+use lib 'HTML';
+use lib 'CSS';
+use HTML::Tiny;		#Changed name in directory from Tiny.pm to CTiny.pm
+use CSS::Tiny;
+
+use lib 'Switch';
+use Switch;
 use Block;
 use Style;
 
 #say "Hello";
-#Parsing 
+#Parsing
 
 my $filename = "multli";
 rename($filename.".docx", $filename.".docx.zip");
@@ -86,10 +93,10 @@ push(my @arrname, $docName);
 
 my @fullDocBlockList = (@arrname,  @headBlocks , @docBlocks , @footBlocks);
 
-for (my $i = 0; $i < @fullDocBlockList; $i++)
-{
-    say $fullDocBlockList[$i]->getStyle()->getFont();
-}
+#for (my $i = 0; $i < @fullDocBlockList; $i++)
+#{
+#    say $fullDocBlockList[$i]->getStyle_id();
+#}
 
 
 #for (my $i = 0; $i < @headBlocks; $i++)
@@ -100,6 +107,8 @@ for (my $i = 0; $i < @fullDocBlockList; $i++)
 #for (my $i = 0; $i < @footBlocks; $i++) {
 #    say $footBlocks[$i]->getText();
 #}
+
+HTMLgen(@fullDocBlockList);
 
 closing();
 
@@ -182,7 +191,7 @@ sub parseDoc
                 $set = 1;
             }
         }
-
+        
         
         my $txt = "";
         foreach my $text  ($wp->findnodes('./w:r/w:t'))
@@ -192,12 +201,12 @@ sub parseDoc
         
         $temp->setText( $txt);
         $temp->setStyle($styleTemp);
-#        my $t = \$temp;
+        #        my $t = \$temp;
         push(@blockList, $temp);
-#        $temp->setText("");
-#        $temp->setStyle_id("");
+        #        $temp->setText("");
+        #        $temp->setStyle_id("");
     }
-
+    
     return @blockList;
 }
 
@@ -222,108 +231,131 @@ sub closing
 
 ###################################################################################
 
-use lib 'HTML';
-use lib 'CSS';
-use HTML::Tiny;		#Changed name in directory from Tiny.pm to CTiny.pm
-use CSS::Tiny;
 
-use lib 'Switch';
-use Switch;
 
-my $html_potato = HTML::Tiny->new( mode =>'html' );
-my @blockList;
-my $htmlList;
-my @htmlBody;
-my @htmlHead;
-my @htmlFoot;
-my $tomato;
-
-my @cssList;
-
-#PARSING BLOCKLIST
-
-foreach my $i (@blockList)
+sub HTMLgen
 {
-    my $temp = $i->getStyle_id();
+    my $html_potato = HTML::Tiny->new( mode =>'html' );
+    my @blockList = @_;
+    my $htmlList;
+    my @htmlBody;
+    my @htmlHead;
+    my @htmlFoot;
+    my $tomato;
+    my $temp;
+    my $filename;
     
-    switch($temp)
+    
+    for (my $i = 0; $i < @blockList; $i++)
     {
-        case "Name"
-        {
-            push( @htmlHead , $html_potato->title($i->getText()));
-        }
-        case "Title"
-        {
-            push (@htmlBody,  $html_potato->h1($i->getText()));
-        }
-        case "Heading1"
-        {
-            push(@htmlBody ,  $html_potato->h2($i->getText()));
-        }
-        case "Heading2"
-        {
-            push (@htmlBody ,  $html_potato->h3($i->getText()));
-        }
-        case "Heading3"
-        {
-            push (@htmlBody , $html_potato->h4($i->getText()));
-        }
-        case "Header"
-        {
-            push (@htmlHead, $html_potato->header($i->getText()));
-        }
-        case "Footer"
-        {
-            push (@htmlFoot, $html_potato->p( $i->getText()));
-        }
-        case "Normal"
-        {
-            push (@htmlBody, $html_potato->p($i->getText()));
-        }
-        else {}
+        say $blockList[$i]->getStyle_id();
     }
+
+    
+    #PARSING BLOCKLIST
+    
+    foreach my $i (@blockList)
+    {
+        $temp = $i->getStyle_id();
+        
+        switch($temp)
+        {
+            case "Name"
+            {
+                $filename = $i->getText();
+                push( @htmlHead , $html_potato->title($i->getText()));
+            }
+            case "Title"
+            {
+                push (@htmlBody,  $html_potato->h1($i->getText()));
+            }
+            case "Heading1"
+            {
+                push(@htmlBody ,  $html_potato->h2($i->getText()));
+            }
+            case "Heading2"
+            {
+                push (@htmlBody ,  $html_potato->h3($i->getText()));
+            }
+            case "Heading3"
+            {
+                push (@htmlBody , $html_potato->h4($i->getText()));
+            }
+            case "Header"
+            {
+                push (@htmlHead, $html_potato->tag("header", $i->getText()));
+            }
+            case "Footer"
+            {
+                push (@htmlFoot, $html_potato->p( $i->getText()));
+            }
+            case "Normal"
+            {
+                push (@htmlBody, $html_potato->p($i->getText()));
+            }
+            else {}
+        }
+    }
+    
+    # HTML HEADER
+    
+#    say $html_potato->tag ('link', { rel => 'stylesheet' }, { href => $filename.".css" });
+    
+    $temp = $htmlHead[0] ."\n".$html_potato->tag ('link', { rel => 'stylesheet' }, { href => $filename.".css" });
+    
+    if (defined($temp))
+    {
+        for my $k (1 .. $#htmlHead)
+        {
+            $temp = $temp."\n".$htmlHead[$k];
+        }
+        
+        $htmlList = $htmlList . $html_potato->head (["\n".$temp."\n"]);
+    }
+    else { say "Error in initialization of Header\n"} ;
+    
+    # HTML BODY
+    
+    $temp = $htmlBody[0];
+    
+    if (defined($temp))
+    {
+        
+        
+        for my $j (1 .. $#htmlBody)
+        {
+            $temp = $temp."\n".$htmlBody[$j];
+        }
+        
+        $htmlList = $htmlList . $html_potato->body (["\n".$temp]);
+    }
+    else { say "Error or no html body\n" };
+    
+    # HTML FOOTER
+    
+    $temp = $htmlFoot[0];
+    
+    if (defined($temp))
+    {
+        for my $l (1 .. $#htmlFoot)
+        {
+            $temp = $temp."\n".$htmlFoot[$l];
+        }
+        
+        $tomato = $tomato . $html_potato->tag("footer",["\n".$temp]);
+        
+    }
+    else { say "Error or no footer\n" };
+    
+    #PRINTING IN HTML FILE
+    
+    open( my $file, '>', $filename.'.html');
+    
+    print $file $html_potato->html(["\n".$htmlList, $tomato."\n"]);
+    
+    close $file;
+    
 }
-
-# HTML HEADER
-
-my $temp;
-
-$temp = $htmlHead[0];
-
-for my $k (1 .. $#htmlHead)
-{
-    $temp = $temp."\n".$htmlHead[$k];
-}
-
-$htmlList = $htmlList . $html_potato->head (["\n".$temp."\n"]);
-
-# HTML BODY
-$temp = $htmlBody[0];
-
-for my $j (1 .. $#htmlBody)
-{
-    $temp = $temp."\n".$htmlBody[$j];
-}
-
-$htmlList = $htmlList . $html_potato->body (["\n".$temp]);
-
-# HTML FOOTER
-$temp = $htmlFoot[0];
-
-for my $l (1 .. $#htmlFoot)
-{
-    $temp = $temp."\n".$htmlFoot[$l];
-}
-
-$tomato = $tomato . $html_potato->tag("footer",["\n".$temp]);
-
-#PRINTING IN HTML FILE
-
-open( my $file, '>', 'test.html');
-
-print $file $html_potato->html(["\n".$htmlList, $tomato."\n"]);
-
-close $file;
 
 
 ###########################################
